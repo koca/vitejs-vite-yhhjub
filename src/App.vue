6e3c0cee64de3ff6e1384c27b0c0298ed7c4a653
koca <imesutkoca@gmail.com>
2021-11-25 18:52:59 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-screen bg-gray-200">
+    <div class="max-w-4xl mx-auto p-4 flex flex-col space-y-8">
+      <h1 class="text-xl text-center">Google Tag Manager Tests</h1>
+      <div class="example shadow-md bg-white p-4 rounded">
+        <h2 class="mb-4">Test Account</h2>
+        <div class="flex flex-wrap space-x-4">
+          <c-button>trigger page_load event</c-button>
+          <c-button>trigger click event</c-button>
+        </div>
+      </div>
+      <div class="example shadow-md bg-white p-4 rounded">
+        <h2 class="mb-4">MM Account</h2>
+        <div class="flex flex-wrap space-x-4">todo</div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/CButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <button
+    class="
+      px-4
+      py-2
+      bg-blue-500
+      rounded-full
+      text-base
+      hover:bg-blue-600
+      text-white
+    "
+  >
+    <slot />
+  </button>
+</template>
